dbbe1e8beb753ff6bc6065581214ed5a12fd9a10
John Nelson <jmn3lson@gmail.com>
2020-04-20 02:03:50 -0500

    Update fibtests.py
    test

--- fibtests.py
+++ fibtests.py
@@ -10,5 +10,8 @@ class TestAssign1(unittest.TestCase):
 
     def test_fib3(self):
         self.assertEqual(fib.fib(25), 75025)
+        
+    def test_fibfoo(self):
+        self.assertEqual(fib.fibnoreclist(8), [1, 1, 2, 3, 5, 8, 13, 21])
 if __name__ ==  "__main__":
         unittest.main()
